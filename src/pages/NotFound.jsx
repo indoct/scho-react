b6c10d7db474746ed8023949a504872009d2a05a
syoban <syoban@gmail.com>
2023-05-05 16:03:28 +0100

    404 component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <>
+      <section class="container">
+        <div class="row justify-content-center text-center mt-5">
+          <div class="col-12 col-lg-6">
+            <h1 class="gradient mb-4 smallerh1">404 (This page doesn't exist!)</h1>
+            <p class="pb-3 pb-sm-5">Unfortunately, the page you're trying to access doesn't exist.</p>
+            <p>
+              You can let me know by visiting
+              <Link to="/contact">Beyond Bitesize</Link>
+            </p>
+            <p>
+              or sending me an email at:
+              <a href="mailto:hello@scho.pro" class="email-big gradient">
+                hello@scho.pro
+              </a>
+            </p>
+            <p>
+              In the meantime, <Link to="/">go back to the homepage</Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
