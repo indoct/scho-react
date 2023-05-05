697aee33c776e0224e3612d8dc99bb10ca2b1157
syoban <syoban@gmail.com>
2023-05-05 19:01:55 +0100

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,8 +6,8 @@ export default function NotFound() {
       <section class="container">
         <div class="row justify-content-center text-center mt-5">
           <div class="col-12 col-lg-6">
-            <h1 class="gradient mb-4 smallerh1">404</h1>
-            <p class="pb-3 pb-sm-5">Unfortunately, the page you're trying to access doesn't exist.</p>
+            <h1 class="gradient mb-4">404</h1>
+            <p class="pb-3 pb-sm-3">Unfortunately, the page you're trying to access doesn't exist.</p>
             {/* <p>
               You can let me know on the <Link to="/contact">contact page</Link>, or by sending me an email at:
               <a href="mailto:hello@scho.pro" class="gradient">
