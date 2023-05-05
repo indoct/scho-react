1c2a10a4ef7a357222e2b06d16cc7b0b1eb875e6
syoban <syoban@gmail.com>
2023-05-05 18:57:04 +0100

    fix layout 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -9,17 +9,13 @@ export default function NotFound() {
             <h1 class="gradient mb-4 smallerh1">404</h1>
             <p class="pb-3 pb-sm-5">Unfortunately, the page you're trying to access doesn't exist.</p>
             <p>
-              You can let me know by visiting
-              <Link to="/contact">Beyond Bitesize</Link>
-            </p>
-            <p>
-              or sending me an email at:
-              <a href="mailto:hello@scho.pro" class="email-big gradient">
+              You can let me know on the <Link to="/contact">contact page</Link>, or by sending me an email at:
+              <a href="mailto:hello@scho.pro" class="gradient">
                 hello@scho.pro
               </a>
             </p>
             <p>
-              In the meantime, <Link to="/">go back to the homepage</Link>
+              In the meantime, go <Link to="/"> back to the homepage</Link>
             </p>
           </div>
         </div>
