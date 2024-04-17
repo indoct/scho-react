1a0312576a128ed597d485b96d59c593479fe7e4
syoban <syoban@gmail.com>
2024-04-17 15:39:55 +0100

    layout changes, navbar improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,124 +3,122 @@ import Logo from "../images/schogo.png";
 
 export default function Navbar() {
   return (
-    <nav
-      className="navbar pt-4 navbar-dark justify-content-between fixed-top"
-      id="menu"
-    >
+    <nav className="navbar pt-4 navbar-dark fixed-top" id="menu">
       <div className="container">
-        <NavLink to="/">
-          <img
-            src={Logo}
-            className="navbar-brand"
-            alt="scho.pro"
-            width="60"
-            height="60"
-          />
-        </NavLink>
-        <div className="nav-cont"></div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ">
-            <NavLink to="/" className="nav-link g1">
-              <li className="nav-item">Home</li>
-            </NavLink>
-            <li className="nav-item project-nav">
-              <div className="accordion">
-                <div className="accordion-item">
-                  <p className="accordion-header align-items-end nav-link">
-                    <button
-                      className="accordion-button collapsed"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseTwo"
-                      aria-expanded="false"
-                      aria-controls="collapseTwo"
+        <div className="logo-cont">
+          <NavLink to="/">
+            <img
+              src={Logo}
+              className="navbar-brand"
+              alt="scho.pro"
+              width="60"
+              height="60"
+            />
+          </NavLink>
+        </div>
+        <div className="nav-cont d-flex align-items-end">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link g1">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item project-nav">
+                <div className="accordion">
+                  <div className="accordion-item">
+                    <p className="accordion-header align-items-end nav-link">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseTwo"
+                        aria-expanded="false"
+                        aria-controls="collapseTwo"
+                      >
+                        Projects
+                      </button>
+                    </p>
+                    <div
+                      id="collapseTwo"
+                      className="proj-nav-collapse collapse"
+                      data-bs-parent="#proj-navExample"
                     >
-                      Projects
-                    </button>
-                  </p>
-                  <div
-                    id="collapseTwo"
-                    className="proj-nav-collapse collapse"
-                    data-bs-parent="#proj-navExample"
-                  >
-                    <ul className="proj-nav-body">
-                      <li className="sub-li">
-                        <NavLink
-                          className="nav-link g1 sub-link"
-                          to="/work/esfjoinus"
-                        >
-                          Join Us (ESF)
-                        </NavLink>
-                        <NavLink
-                          className="nav-link g1 sub-link"
-                          to="/work/beyondbitesize"
-                        >
-                          Beyond Bitesize
-                        </NavLink>
-                      </li>
-                      <li className="sub-li">
-                        <NavLink
-                          className="nav-link g2 sub-link"
-                          to="/work/scho"
-                        >
-                          Personal Identity (scho.)
-                        </NavLink>
-                      </li>
-                      <li className="sub-li">
-                        <NavLink
-                          className="nav-link g3 sub-link"
-                          to="/work/growbuddy"
-                        >
-                          Grow Buddy
-                        </NavLink>
-                      </li>
-                      <li className="sub-li">
-                        <NavLink
-                          className="nav-link g4 sub-link"
-                          to="/work/theodddystrict"
-                        >
-                          The Odd Dystrict
-                        </NavLink>
-                      </li>
-                      <li className="sub-li">
-                        <NavLink
-                          className="nav-link g1 sub-link"
-                          to="/work/juno"
-                        >
-                          Juno Bakeshop
-                        </NavLink>
-                      </li>
-                    </ul>
+                      <ul className="proj-nav-body">
+                        <li className="sub-li">
+                          <NavLink
+                            className="nav-link g1 sub-link"
+                            to="/work/esfjoinus"
+                          >
+                            Join Us (ESF)
+                          </NavLink>
+                          <NavLink
+                            className="nav-link g1 sub-link"
+                            to="/work/beyondbitesize"
+                          >
+                            Beyond Bitesize
+                          </NavLink>
+                        </li>
+                        <li className="sub-li">
+                          <NavLink
+                            className="nav-link g2 sub-link"
+                            to="/work/scho"
+                          >
+                            Personal Identity
+                          </NavLink>
+                        </li>
+                        <li className="sub-li">
+                          <NavLink
+                            className="nav-link g3 sub-link"
+                            to="/work/growbuddy"
+                          >
+                            Grow Buddy
+                          </NavLink>
+                        </li>
+                        <li className="sub-li">
+                          <NavLink
+                            className="nav-link g4 sub-link"
+                            to="/work/theodddystrict"
+                          >
+                            The Odd Dystrict
+                          </NavLink>
+                        </li>
+                        <li className="sub-li">
+                          <NavLink
+                            className="nav-link g1 sub-link"
+                            to="/work/juno"
+                          >
+                            Juno Bakeshop
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link g3" to="/about">
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link g4" to="/contact">
-                Contact
-              </NavLink>
-            </li>
-            {/* <li className="nav-item d-flex flex-row justify-content-end">
-              <a className="nav-link g1"><i className="fa-brands fa-square-instagram"></i></a>
-              <a className="nav-link g2"><i className="fa-brands fa-square-behance"></i></a>
-            </li>  */}
-          </ul>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link g3" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link g4" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,149 +16,119 @@ export default function Home() {
   }, []);
   return (
     <>
-      <section className="container header home">
-        <div className="row mt-5">
-          <div className="col col-lg-6">
-            <h1 className="aos-init aos-animate" data-aos="zoom-in">
-              <span className="underline gradient">scho.</span>
-            </h1>
-            <p
-              className="intro aos-init aos-animate"
-              data-scroll=""
-              data-aos="fade-up"
-              data-aos-duration="1000"
-            >
-              Welcome to the design portfolio of London based designer SC
-              Houlihan. I like to craft attractive, useful things and
-              experiences.
-            </p>
+      <div className="container">
+        <section className="header home">
+          <div className="row">
+            <div className="col col-xl-6">
+              <h1
+                data-aos="fade-up"
+                data-aos-delay="100"
+                data-aos-duration="1200"
+              >
+                <span className="underline gradient">scho.</span>
+              </h1>
+              <p
+                className="intro"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="200"
+              >
+                Welcome to the design portfolio of London based designer SC
+                Houlihan. I like to craft attractive, useful things and
+                experiences.
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-      <section id="case-studies">
-        <div className="container">
-          <article
-            className="row project"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-          >
-            <Link to="work/beyondbitesize">
-              <div className="col-12 text-center">
-                <img
-                  src={bbscreen}
-                  alt="Beyond Bitesize Screen 1"
-                  className="img-fluid bb"
-                />
-              </div>
-
-              <div className="col-12">
-                <h2 className="p-title">Beyond Bitesize</h2>
-                <p className="p-desc">
-                  An engaging and inclusive learning platform
-                </p>
-              </div>
-            </Link>
-          </article>
-        </div>
-
-        <div className="container">
-          <article
-            className="row project"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-          >
-            <Link to="work/scho">
-              <div className="col-12 text-center">
-                <img
-                  src={schopb}
-                  alt="scho. business card"
-                  className="img-fluid bb"
-                />
-              </div>
-
-              <div className="col-12">
-                <h2 className="p-title">Personal Branding (scho.)</h2>
-                <p className="p-desc">
-                  Rebranding and collateral of my design identity
-                </p>
-              </div>
-            </Link>
-          </article>
-        </div>
-
-        <div className="container">
-          <article
-            className="row project"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-          >
-            <Link to="work/growbuddy">
-              <div className="col-12 text-center">
-                <img
-                  src={gbscreen}
-                  alt="Grow Buddy Pack"
-                  className="img-fluid bb"
-                />
-              </div>
-
-              <div className="col-12">
-                <h2 className="p-title">Grow Buddy</h2>
-                <p className="p-desc">
-                  A pack & companion app for growing microgreens at home
-                </p>
-              </div>
-            </Link>
-          </article>
-        </div>
-        <div className="container">
-          <article
-            className="row project"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-          >
-            <Link to="work/theodddystrict">
-              <div className="col-12 text-center">
-                <img
-                  src={todscreen}
-                  alt="A screencap of the homepage"
-                  className="img-fluid bb"
-                />
-              </div>
-
-              <div className="col-12">
-                <h2 className="p-title">The Odd Dystrict</h2>
-                <p className="p-desc">
-                  Identity & UI design for an NFT project
-                </p>
-              </div>
-            </Link>
-          </article>
-        </div>
-        <div className="container">
-          <article
-            className="row project"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-          >
-            <Link to="work/juno">
-              <div className="col-12 text-center">
-                <img
-                  src={junobag}
-                  alt="Juno packaging with stamp logo"
-                  className="img-fluid bb"
-                />
-              </div>
-
-              <div className="col-12">
-                <h2 className="p-title">Juno Bakeshop</h2>
-                <p className="p-desc">
-                  Identity design for a small home bakery business
-                </p>
-              </div>
-            </Link>
-          </article>
-        </div>
-      </section>
+        </section>
+        {/* <div className="container">
+          <div className="row gx-5">
+            <div className="col test">1</div>
+            <div className="col test">2</div>
+            <div className="col test">3</div>
+          </div>
+        </div> */}
+        <section id="case-studies" data-aos="fade-in" data-aos-duration="1200">
+          <div className="row work-row mb-2 g-2">
+            <div className="col-12 col-md-6 col-xl-4">
+              <article className="project py-4">
+                <Link to="work/beyondbitesize" className="text-center">
+                  <img
+                    src={bbscreen}
+                    alt="Beyond Bitesize Screen 1"
+                    className="img-fluid bb"
+                  />
+                  <h2 className="p-title">Beyond Bitesize</h2>
+                  <p className="p-desc">
+                    An engaging and inclusive learning platform
+                  </p>
+                </Link>
+              </article>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <article className="project py-4">
+                <Link to="work/scho" className="text-center">
+                  <img
+                    src={schopb}
+                    alt="scho. business card"
+                    className="img-fluid bb"
+                  />
+                  <h2 className="p-title">Personal Branding (scho.)</h2>
+                  <p className="p-desc">
+                    Rebranding and collateral of my design identity
+                  </p>
+                </Link>
+              </article>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <article className="project py-4">
+                <Link to="work/growbuddy" className="text-center">
+                  <img
+                    src={gbscreen}
+                    alt="Grow Buddy Pack"
+                    className="img-fluid bb"
+                  />
+                  <h2 className="p-title">Grow Buddy</h2>
+                  <p className="p-desc">
+                    A pack & companion app for growing microgreens
+                  </p>
+                </Link>
+              </article>
+            </div>
+            {/* </div>
+          <div className="row work-row mb-2 gx-2"> */}
+            <div className="col-12 col-md-6 col-xl-4">
+              <article className="project py-4">
+                <Link to="work/theodddystrict">
+                  <img
+                    src={todscreen}
+                    alt="A screencap of the homepage"
+                    className="img-fluid bb"
+                  />
+                  <h2 className="p-title">The Odd Dystrict</h2>
+                  <p className="p-desc">
+                    Identity & UI design for an NFT project
+                  </p>
+                </Link>
+              </article>
+            </div>
+            <div className="col-12 col-md-6 col-xl-4">
+              <article className="project py-4">
+                <Link to="work/juno">
+                  <img
+                    src={junobag}
+                    alt="Juno packaging with stamp logo"
+                    className="img-fluid bb"
+                  />
+                  <h2 className="p-title">Juno Bakeshop</h2>
+                  <p className="p-desc">
+                    Identity design for a small home bakery business
+                  </p>
+                </Link>
+              </article>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
