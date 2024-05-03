42e09051fc55f53e86b30d4198b8852ea9137eea
syoban <syoban@gmail.com>
2024-05-04 00:51:35 +0100

    styling loss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className="container">
       <section className="header home row">
-        <div className="col col-xl-6">
+        <div className="col col-xl-7">
           <h1 data-aos="fade-up" data-aos-delay="100" data-aos-duration="1200">
             <span className="underline gradient">scho.</span>
           </h1>
@@ -39,7 +39,7 @@ export default function Home() {
         data-aos="fade-up"
         data-aos-delay="300"
         data-aos-duration="1000"
-        className="row work-row mb-2 g-2"
+        className="row work-row mb-2 g-4"
       >
         <li className="col-12 col-md-6 col-xl-4 proj-card">
           <div className="card-img">
