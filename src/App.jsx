6cf335eb5ad80076fe298eb769db949ea2a9f1c9
syoban <syoban@gmail.com>
2024-05-07 10:48:05 +0100

    Update App.jsx
    netlify still saying React is not defined.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
