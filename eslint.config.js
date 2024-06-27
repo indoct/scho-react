1318ad05e0a96f336e7c6ab80e6355fc6efd245c
syoban <syoban@gmail.com>
2024-06-27 22:10:10 +0100

    module cleanup, linting config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,24 @@
+const react = require('eslint-plugin-react-refresh');
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/jsx-runtime.js";
+
 export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  { plugins: [ react ] },
   {
     rules: {
-      semi: "error",
-      "prefer-const": "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import svgr from "vite-plugin-svgr";
-
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-//   build: {
-//     outDir: "build",
-//   },
-//   loader: { ".js": "jsx" },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react(), eslint()],
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    outDir: "build",
+    minify: "esbuild",
   },
+  plugins: [react()],
 });
